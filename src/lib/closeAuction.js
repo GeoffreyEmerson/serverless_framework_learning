be5608f52199f181e959e643105adbdcdafa4f94
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2020-07-01 20:41:07 -0700

    add messages to auction close event

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
     const params = {
@@ -15,6 +16,41 @@ export async function closeAuction(auction) {
         }
     };
 
-    const result = await dynamodb.update(params).promise();
-    return result;
+    await dynamodb.update(params).promise();
+
+    const { title, seller, highestBid } = auction;
+    const { amount, bidder } = highestBid;
+
+    if (bidder && amount > 0) {
+        const notifySeller = sqs.sendMessage({
+            QueueUrl: process.env.MAIL_QUEUE_URL,
+            MessageBody: JSON.stringify({
+                recipient: seller,
+                subject: 'Your item has been sold!',
+                body: `Success! Your item "${title}" has sold for $${amount}.`
+            })
+        }).promise();
+    
+        const notifyBidder = sqs.sendMessage({
+            QueueUrl: process.env.MAIL_QUEUE_URL,
+            MessageBody: JSON.stringify({
+                recipient: bidder,
+                subject: 'You won an auction!',
+                body: `By Grapthar's Hammer, what savings. You won the auction for "${title}" for $${amount}.`
+            })
+        }).promise();
+    
+        return Promise.all([notifySeller, notifyBidder]);
+    }
+
+    await sqs.sendMessage({
+        QueueUrl: process.env.MAIL_QUEUE_URL,
+        MessageBody: JSON.stringify({
+            recipient: seller,
+            subject: 'Your item expired with no bids.',
+            body: `Sorry! Your item "${title}" did not find a buyer. Never give up, never surrender!`
+        })
+    }).promise();
+
+    return;
 }
